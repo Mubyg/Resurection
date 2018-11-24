e3375ddfd30b7a315c28ea0b50c93ab419bcb82b
Mubyg <mubyg9@gmail.com>
2018-11-24 15:33:19 +0100

    Basics (floor, character, movement)

---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float MovementSpeed = 0.1f;
+    [SerializeField]
+    private float KeyRotationSpeed = 0.1f;
+    [SerializeField]
+    private float MouseRotationSpeed = 0.1f;
+    [SerializeField]
+    private Transform CameraTransform = null;
+
+    void Start() {
+        Cursor.visible = false;
+    }
+
+    void Update() {
+        float forward = Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime;
+        float right = Input.GetAxis("Horizontal") * KeyRotationSpeed * Time.deltaTime;
+        float rotationX = Input.GetAxis("Mouse X") * MouseRotationSpeed * Time.deltaTime;
+        float rotationY = Input.GetAxis("Mouse Y") * MouseRotationSpeed * Time.deltaTime;
+
+        transform.Translate(right, 0, forward);
+        transform.Rotate(0, rotationX, 0);
+        CameraTransform.Rotate(-rotationY, 0, 0);
+    }
+}
